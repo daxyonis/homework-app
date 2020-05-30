8d7c79e7b98a959ecf15ee63944460f746513671
daxyonis <emaciejko@gmail.com>
2020-05-30 16:18:32 -0400

    Cutomize colors

--- frontend/components/TeacherView.vue
+++ frontend/components/TeacherView.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="pt-2">
     <div class="float-md-right">
-        <button type="button" class="btn btn-primary onem">
+        <button type="button" class="btn btn-success onem">
             <svg class="bi bi-plus-circle-fill vam" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4a.5.5 0 0 0-1 0v3.5H4a.5.5 0 0 0 0 1h3.5V12a.5.5 0 0 0 1 0V8.5H12a.5.5 0 0 0 0-1H8.5V4z"/>
             </svg>
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -12,8 +12,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { 'http-equiv': "X-UA-Compatible", 'content': "IE=edge"}
-    ],    
-    script:[
+    ],   
+    link:[
+      {href:"https://use.fontawesome.com/releases/v5.0.10/css/all.css", rel:"stylesheet"},
+      {href:"https://fonts.googleapis.com/css?family=Muli:200,300,400,700", rel:"stylesheet"},
+      {href:"https://fonts.googleapis.com/css?family=Comfortaa:200,300,400,700", rel:"stylesheet"}
+    ] ,
+    script:[      
       {src:"https://code.jquery.com/jquery-3.5.1.slim.min.js", integrity:"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj", crossorigin:"anonymous"},
       {src:"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", integrity:"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo", crossorigin:"anonymous"},
       {src:"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js", integrity:"sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI", crossorigin:"anonymous"}
@@ -26,8 +31,9 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
-    '@/assets/css/bootstrap.css'
+  css: [    
+    '@/assets/vendor/bootstrap/css/bootstrap.css',
+    '@/assets/css/theme_1590869380461.css'
   ],
   /*
   ** Plugins to load before mounting the App
